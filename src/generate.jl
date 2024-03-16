e709c33860c68d5c728167cdb5cf228a6bf528fe
Albert <albert03@mit.edu>
2024-03-16 14:24:05 -0400

    dense generator

--- src/generate.jl
+++ src/generate.jl
@@ -65,7 +65,6 @@ module Utils
 end
 
 module Generate
-
     using Comonicon
 
     """
@@ -117,6 +116,70 @@ module Generate
             Finch.fwrite(joinpath(out, "y_ref.$ext"), copyto!(Tensor(Dense(Element(0.0))), y))
             Finch.fwrite(joinpath(out, "A.$ext"), copyto!(swizzle(Tensor(Dense(SparseList(Element(0.0)))), 2, 1), A))
             Finch.fwrite(joinpath(out, "x.$ext"), copyto!(Tensor(Dense(Element(0.0))), x))
+        end
+
+        vuduc02_matrices = [
+            "Simon/raefsky3", "Simon/olafu", "Boeing/bcsstk35", "Simon/venkat01", "Boeing/crystk02",
+            "Boeing/crystk03", "Nasa/nasasrb", "Rothberg/3dtube", "Boeing/ct20stif", "Bai/af23560",
+            "Simon/raefsky4", "FIDAP/ex11", "Zitney/rdist1", "Vavasis/av41092", "HB/orani678",
+            "Goodwin/rim", "Hamm/memplus", "HB/gemat11", "Mallya/lhr10", "Goodwin/goodwin",
+            "Grund/bayer02", "Grund/bayer10", "Brethour/coater2", "Mulvey/finan512", "ATandT/onetone2",
+            "Nasa/pwt", "Cote/vibrobox", "Wang/wang4", "HB/lnsp3937", "HB/lns_3937", "HB/sherman5",
+            "HB/sherman3", "HB/orsreg_1", "HB/saylr4", "Shyy/shyy161", "Wang/wang3", "HB/mcfe",
+            "HB/jpwh_991", "Gupta/gupta1", "LPnetlib/lp_cre_b", "LPnetlib/lp_cre_d", "LPnetlib/lp_fit2p",
+            "Qaplib/lp_nug20"
+        ]
+        """
+        generate spmv vuduc02
+        
+        generate an instance of SpMV from the SuiteSparse matrix collection
+        
+        # Intro
+
+        generate an instance of SpMV from the SuiteSparse matrix collection
+        
+        # Options
+        
+        - `-o, --out=</data>`: destination directory for the generated problem instances
+        - `-e, --ext <extension>`: generated tensor file format extension
+        
+        """
+        @cast function vuduc02(out = joinpath(@__DIR__, "../data"), ext="bspnpy")
+            for matrix_key in vuduc02_matrices
+                suitesparse(matrix_key; out = joinpath(out, matrix_key), ext=ext)
+            end
+
+        end
+
+        langr_matrices = [
+            "Buss/12month1", "Sinclair/3Dspectralwave2", "Schenk_AFE/af_shell10", "SNAP/amazon0312",
+            "Bourchtein/atmosmodj", "GHS_psdef/bmw7st_1", "vanHeukelum/cage12", "vanHeukelum/cage15",
+            "Botonakis/FEM_3D_thermal2", "Lee/fem_hifreq_circuit", "Freescale/Freescale1", "Freescale/FullChip",
+            "LAW/hollywood-2009", "LAW/in-2004", "GHS_psdef/ldoor", "Belcastro/mouse_gene", "Schenk/nlpkkt120",
+            "Schenk_ISEI/ohne2", "Mittelmann/rail4284", "Rajat/rajat31", "JGD_Relat/relat9", "Fluorem/RM07R",
+            "Rucci/Rucci1", "Mittelmann/spal_004", "Schmid/thermal2", "TSOPF/TSOPF_RS_b2383", "Gleich/wb-edu",
+            "Gleich/wikipedia-20061104", "VanVelzen/Zd_Jac2"
+        ]
+        """
+        generate spmv langr
+        
+        generate an instance of SpMV from the SuiteSparse matrix collection
+        
+        # Intro
+
+        generate an instance of SpMV from the SuiteSparse matrix collection
+        
+        # Options
+        
+        - `-o, --out=</data>`: destination directory for the generated problem instances
+        - `-e, --ext <extension>`: generated tensor file format extension
+        
+        """
+        @cast function langr(out = joinpath(@__DIR__, "../data"), ext="bspnpy")
+            for matrix_key in langr_matrices
+                suitesparse(matrix_key; out = joinpath(out, matrix_key), ext=ext)
+            end
+
         end
 
         """
