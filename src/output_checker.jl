919d5e7afc8f5b2f14a3009d0598154576a40aa5
Albert <albert03@mit.edu>
2024-04-26 10:26:55 -0400

    output_checker finished

--- src/output_checker.jl
+++ src/output_checker.jl
@@ -1,28 +1,44 @@
-import Pkg;
-Pkg.add("Finch")
-Pkg.add("NPZ")
-Pkg.add("TensorMarket")
-if length(ARGS) != 3
-    println("Usage: julia output_checker.jl <A.ext> <x.ext> <y_ref.ext>")
+if abspath(PROGRAM_FILE) == @__FILE__
+    using Pkg
+    Pkg.activate(@__DIR__, io = devnull)
+    Pkg.instantiate()
+end
+
+if length(ARGS) != 5
+    println("Usage: julia output_checker.jl <A.ext> <x.ext> <y.ext> <y_ref.ext> epsilon")
     exit(1)
 end
 
 using Finch
 using NPZ
 using TensorMarket
+
+flag = Scalar(true)
+y_bound = Tensor(Dense(Element(0.0)))
 A = fread(ARGS[1])
 x = fread(ARGS[2])
-y_ref = fread(ARGS[3])
+y = fread(ARGS[3])
+y_ref = fread(ARGS[4])
+epsilon = fread(ARGS[5])
 (m, n) = size(A)
 
-println(size(A))
-println(size(x))
-println(size(y_ref))
-
-y = A*x
-
-if (y!=y_ref)
-    error("output does not match")
-else
-    println("matches output")
+@finch begin
+    y_bound .= 0
+    for j = _
+        for i = _
+            @einsum y_bound[i] += abs(A[i, j]*x[j])
+        end
+    end
+    for i = _
+        @einsum flag[] &= (abs(y_ref[i] - y[i]) <= n*epsilon*y_bound[i])
+    end
+    return flag
 end
+
+
+if (flag == Scalar(true))
+    println("output is correct")
+else
+    error("y doesn't match y_ref")
+end
+
